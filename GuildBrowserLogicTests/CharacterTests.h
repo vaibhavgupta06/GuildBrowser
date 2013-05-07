ded68b375f38a58a32974e5618a72a11b065e2a2
vaibhav <vaibhavgupta06@gmail.com>
2013-05-07 17:52:10 +0530

    Implemented test cases for Character class

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Vaibhav Gupta on 07/05/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
